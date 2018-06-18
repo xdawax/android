52be2ca6362fd9f4ded5f30feadac2a0f64f0761
xdawax <dawa1980@gmail.com>
2018-06-18 13:15:57 +0200

    Add saving die when destroying the view

--- SaveStateExercise/app/src/main/java/se/umu/cs/johane/savestateexercise/Die.java
+++ SaveStateExercise/app/src/main/java/se/umu/cs/johane/savestateexercise/Die.java
@@ -1,5 +1,8 @@
 package se.umu.cs.johane.savestateexercise;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Random;
 
 /**
@@ -7,7 +10,18 @@ import java.util.Random;
  * A representation of a 6 sided Die
  */
 
-public class Die {
+public class Die implements Parcelable{
+
+    public static final Parcelable.Creator<Die> CREATOR = new Parcelable.Creator<Die>() {
+        public Die createFromParcel(Parcel in) {
+            return new Die(in);
+        }
+
+        public Die[] newArray(int size) {
+            return new Die[size];
+        }
+    };
+
     private int value;
     private Random rand;
 
@@ -22,5 +36,19 @@ public class Die {
 
     public void roll() {
         value=rand.nextInt(6)+1;
+    }
+
+    public Die(Parcel in){
+        this.value = in.readInt();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.value);
     }
 }
--- SaveStateExercise/app/src/main/java/se/umu/cs/johane/savestateexercise/StateActivity.java
+++ SaveStateExercise/app/src/main/java/se/umu/cs/johane/savestateexercise/StateActivity.java
@@ -2,6 +2,7 @@ package se.umu.cs.johane.savestateexercise;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -9,12 +10,22 @@ public class StateActivity extends AppCompatActivity {
 
     Die die;
 
+    private final String DIE = "Current die";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_state);
         die=new Die();
+
+
+        if (savedInstanceState != null) {
+            Log.i("savedInstanceStace", "restoring die");
+            this.die = savedInstanceState.getParcelable(DIE);
+        }
+
         setValueText();
+
     }
 
     private void setValueText() {
@@ -25,5 +36,11 @@ public class StateActivity extends AppCompatActivity {
     public void rollDice(View src){
         die.roll();
         setValueText();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(DIE, die);
     }
 }
