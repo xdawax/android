5ef8bbbc2f8a316205442deb6ced182b80a0e9c9
xdawax <dawa1980@gmail.com>
2018-06-27 10:24:04 +0200

    Add getters for CrimeLab

--- CriminalIntent/app/src/main/java/com/example/dawa1/criminalintent/CrimeLab.java
+++ CriminalIntent/app/src/main/java/com/example/dawa1/criminalintent/CrimeLab.java
@@ -1,4 +1,39 @@
 package com.example.dawa1.criminalintent;
 
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class CrimeLab {
+
+    private List<Crime> mCrimes;
+
+    public static CrimeLab sCrimeLab;
+
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId() == id){
+                 return crime;
+            }
+        }
+
+        return null;
+    }
 }
