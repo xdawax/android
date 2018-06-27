2e2d043116b364af05d4bbf65c89bf14e004ceb6
xdawax <dawa1980@gmail.com>
2018-06-27 13:50:49 +0200

    Add recyclerview

--- CriminalIntent/app/src/main/java/com/example/dawa1/criminalintent/CrimeLab.java
+++ CriminalIntent/app/src/main/java/com/example/dawa1/criminalintent/CrimeLab.java
@@ -21,6 +21,13 @@ public class CrimeLab {
 
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0);
+            mCrimes.add(crime);
+        }
     }
 
     public List<Crime> getCrimes() {
---
+++ CriminalIntent/app/src/main/java/com/example/dawa1/criminalintent/CrimeListActivity.java
@@ -0,0 +1,10 @@
+package com.example.dawa1.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
